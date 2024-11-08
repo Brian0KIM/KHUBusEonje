598ec3dd4e7afd9c87a11d7a18b4e56641abaf7b
KIMSEONGMIN <128333586+Brian0KIM@users.noreply.github.com>
2024-11-08 17:35:46 +0900

    minor fix

--- back/index.js
+++ back/index.js
@@ -188,16 +188,22 @@ app.get('/complain/:stationId/passedby', (req, res) => {
 
 app.get('/bus/history', async (req, res) => {
     try {
-        const { routeId, stationId, staOrder, date } = req.query;
+        const { routeId, stationId, date } = req.query;
         
         // 필수 파라미터 검증
-        if (!routeId || !stationId || !staOrder || !date) {
+        if (!routeId || !stationId || !date) {
             return res.status(400).json({
                 ok: false,
                 error: '필수 파라미터가 누락되었습니다'
             });
         }
-
+        const staOrder = database.getStaOrder(stationId, routeId);
+        if (!staOrder) {
+            return res.status(400).json({
+                ok: false,
+                error: '해당 정류장과 노선의 조합이 유효하지 않습니다'
+            });
+        }
         // 날짜 형식 검증 (YYYY-MM-DD)
         const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
         if (!dateRegex.test(date)) {
@@ -230,7 +236,6 @@ app.get('/bus/history/byTime', async (req, res) => {
                 error: '필수 파라미터가 누락되었습니다'
             });
         }
-
         // 날짜 형식 검증 (YYYY-MM-DD)
         const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
         if (!dateRegex.test(date)) {
@@ -241,7 +246,7 @@ app.get('/bus/history/byTime', async (req, res) => {
         }
 
         // 해당 정류장의 모든 버스 노선에 대한 기록 조회
-        const stationRoutes = stationRouteOrder[stationId];
+        const stationRoutes = database.stationRouteOrder[stationId];
         if (!stationRoutes) {
             return res.status(400).json({
                 ok: false,
@@ -251,7 +256,7 @@ app.get('/bus/history/byTime', async (req, res) => {
 
         let allResults = [];
         for (const [routeId, staOrder] of Object.entries(stationRoutes)) {
-            if (busRouteMap[routeId]) {  // busRouteMap에 있는 노선만 처리
+            if (database.busRouteMap[routeId]) {  // busRouteMap에 있는 노선만 처리
                 const result = await database.getPastBusArrival(routeId, stationId, staOrder, date, true);  // true는 7일 전 데이터만 가져오는 플래그
                 if (result.ok && result.data) {
                     allResults = allResults.concat(result.data);
@@ -269,7 +274,7 @@ app.get('/bus/history/byTime', async (req, res) => {
         res.json({
             ok: true,
             data: allResults,
-            stationName: stationMap[stationId],
+            stationName: database.stationMap[stationId],
             lastUpdate: new Date()
         });
 
@@ -285,77 +290,3 @@ app.get('/bus/history/byTime', async (req, res) => {
 
 
 // staOrder 매핑 객체 추가
-const stationRouteOrder = {
-    "228001174":{//사색의광장(정문행)
-        "200000103": "1",//9번 
-        "200000115": "1",//5100
-        "200000112": "1",//7000
-        "234001243": "1",//M5107
-        "234000884": "1",//1560A
-        "234000016": "1",//1112
-        "228000433": "1",//1560B
-    },
-    "228000704":{//생명과학대.산업대학(정문행)
-        "200000103": "2",//9번 
-        "200000115": "2",//5100
-        "200000112": "2",//7000
-        "234001243": "2",//M5107
-        "234000884": "2",//1560A
-        "234000016": "2",//1112
-        "228000433": "2",//1560B
-    },
-    "228000703":{//경희대체육대학.외대(정문행)
-        "200000103": "3",//9번 
-        "200000115": "3",//5100
-        "200000112": "3",//7000
-        "234001243": "3",//M5107
-        "234000884": "3",//1560A
-        "234000016": "3",//1112
-        "228000433": "3",//1560B
-    },
-    "203000125":{//경희대학교(정문행)
-        "200000103": "4",//9번 
-        "200000115": "4",//5100
-        "200000112": "4",//7000
-        "234001243": "4",//M5107
-        "234000884": "4",//1560A
-        "234000016": "4",//1112
-        "228000433": "4",//1560B
-    },
-    "228000723":{//경희대정문(사색행)
-        "200000103": "87",//9번 
-        "200000115": "56",//5100
-        "200000112": "76",//7000
-        "234001243": "60",//M5107
-        "234000884": "99",//1560A
-        "234000016": "68",//1112
-        "228000433": "99",//1560B
-    },
-    "228000710":{//외국어대학(사색행)
-        "200000103": "88",//9번 
-        "200000115": "57",//5100
-        "200000112": "77",//7000
-        "234001243": "61",//M5107
-        "234000884": "100",//1560A
-        "234000016": "69",//1112
-        "228000433": "100",//1560B
-    },
-    "228000709":{//생명과학대(사색행)
-        "200000103": "89",//9번 
-        "200000115": "58",//5100
-        "200000112": "78",//7000
-        "234001243": "62",//M5107
-        "234000884": "101",//1560A
-        "234000016": "70",//1112
-        "228000433": "101",//1560B
-    },
-    "228000708":{//사색의광장(사색행)
-        "200000103": "90",//9번 
-        "200000115": "59",//5100
-        "200000112": "79",//7000
-        "234001243": "63",//M5107
-        "234000884": "102",//1560A
-        "234000016": "71",//1112
-        "228000433": "102",//1560B
-    }
-};
